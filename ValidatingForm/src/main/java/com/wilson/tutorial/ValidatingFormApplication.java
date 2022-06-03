96410e51e3c4f61fcf368d839285a1ddd58c161f
Joe Wilson <wilsonmangunsong@gmail.com>
2022-06-03 22:22:37 +0700

    sec13_vid61
    Validating Form Input

---
+++ ValidatingForm/src/main/java/com/wilson/tutorial/ValidatingFormApplication.java
@@ -0,0 +1,13 @@
+package com.wilson.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatingFormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidatingFormApplication.class, args);
+	}
+
+}
---
+++ ValidatingForm/src/test/java/com/wilson/tutorial/ValidatingFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilson.tutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatingFormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
